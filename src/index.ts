a3c31fbf2499cd07686422382453b3cd33e752b4
oiceotsemoura <ceotcampos@gmail.com>
2023-10-13 02:05:57 -0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Is this working?');
+import 'module-alias/register';
+import express, { Application } from 'express';
+import bodyParser from 'body-parser';
+import TaskController from '@controller/TasksController';
+
+const app: Application = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(TaskController);
+
+try {
+  app.listen(PORT, (): void => {
+    console.log(`Connected successfully on port ${PORT}`);
+  });
+} catch (error: any) {
+  console.error(`Error occured: ${error.message}`);
+}
